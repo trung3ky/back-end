70efb05da571c6df0f8cce0d25a1677623544461
trunghuynh <trungbop26092000@gmail.com>
2022-10-25 22:06:29 +0700

    feat: crud product and rating

--- controller/product.js
+++ controller/product.js
@@ -2,12 +2,13 @@ var bodyParser = require('body-parser');
 var urlencodeParser = bodyParser.urlencoded({ extended: false });
 var parserJson = bodyParser.json();
 var connection = require('./db');
+var TimeNow = require('./atom');
 const uuid = require('uuid');
 
 module.exports = function(app) {
     app.get('/product', function(req, res, next) {
         const max_results = req.query.max_results ?? 25;
-        var sql = `select * from products limit ${max_results}`;
+        var sql = `select * from products where deleted = 0 limit ${max_results}`;
         
         connection.query(sql, function(err, result) {
             if (err) {
@@ -19,17 +20,20 @@ module.exports = function(app) {
     
     app.get('/product/:product_id', function(req, res) {
         const productId = req.params['product_id']
-        var sql = "select * from products where id = '" + productId + "' ";
+        var sql = "select * from products where id = '" + productId + "' and deleted = 0";
         connection.query(sql, function(err, result) {
             if (err) {
                 throw err;
             }
-            res.send(...result);
+            if(result.length > 0){
+                res.send(...result);
+            }else {
+                res.send({message: 'not found'})
+            }
         });
     })
 
-    app.post('/product/create_product', function(req, res) {
-       
+    app.post('/product/create_product', urlencodeParser, function(req, res) {
         const name = req.body.name ?? null;
         const category_id = req.body.category_id ?? null;
         const shop_id = req.body.shop_id ?? null;
@@ -39,16 +43,65 @@ module.exports = function(app) {
         const price = req.body.price ?? null;
         const sale = req.body.sale ?? null;
 
-        var sql = `insert into products(id, brand, slug, id_shop, id_category, name, image, description, quanlity, price, sale, rating, others) 
-        values('${uuid.v4()}', ${null}, ${null}, ${shop_id}, ${category_id}, '${name}', '${image}', '${description}', ${quanlity}, ${price}, ${sale}, ${null}, ${null})`;
+        var sql = `insert into products(id, brand, slug, id_shop, id_category, name, image, description, quanlity, price, sale, rating, others, deleted, created_at, updated_at) 
+        values('${uuid.v4()}', null, null, ${shop_id}, ${category_id}, '${name}', '${image}', '${description}', ${quanlity}, ${price}, ${sale}, null, null, 0, '${TimeNow()}', '${TimeNow()}')`;
 
         connection.query(sql, function(err, result) {
             if (err) {
-                throw err;
+                res.json({type: 'Create error', message: err.message})
+            }else {
+                res.json({type: 'Create success'})
             }
-            res.send(...result);
         });
     })
+
+    app.post('/product/delete_product/:product_id', urlencodeParser, function(req, res) {
+        const productId = req.params['product_id']
+        var sql = `UPDATE products
+        SET deleted = 1
+        WHERE id = '${productId}'`;
+
+        connection.query(sql, function(err, result) {
+            if (err) {
+                res.json({type: 'Delete error', message: err.message})
+            }else {
+                res.json({type: 'Delete success'})
+            }
+        });
+    })
+
+    app.post('/product/update_product/:product_id', urlencodeParser, function(req, res) {
+        const productId = req.params['product_id']
+
+        const name = req.body?.name ?? null;
+        const category_id = req.body?.category_id ?? null;
+        const image = req.body?.image ?? null;
+        const description = req.body?.description ?? null;
+        const quanlity = req.body?.quanlity ?? null;
+        const price = req.body?.price ?? null;
+        const sale = req.body?.sale ?? null;
+        
+        var sql = `UPDATE products
+        SET name =' ${name}',
+        category_id = ${category_id},
+        image =' ${image}',
+        description = '${description}',
+        quanlity = ${quanlity},
+        price = ${price},
+        sale = ${sale},
+        updated_at = '${TimeNow()}'
+        WHERE id = '${productId}'`;
+
+        connection.query(sql, function(err, result) {
+            if (err) {
+                res.json({type: 'Update error', message: err.message})
+            }else {
+                res.json({type: 'Update success'})
+            }
+        });
+    })
+
+
 }
 
 
--- controller/rate.js
+++ controller/rate.js
@@ -2,11 +2,12 @@ var bodyParser = require('body-parser');
 var urlencodeParser = bodyParser.urlencoded({ extended: false });
 var parserJson = bodyParser.json();
 var connection = require('./db');
+var TimeNow = require('./atom');
 
 module.exports = function(app) {
     app.get('/rate', function(req, res, next) {
         const max_results = req.query.max_results ?? 25;
-        var sql = `select * from rating limit ${max_results}`;
+        var sql = `select * from rating where deleted = 0 limit ${max_results}`;
         
         connection.query(sql, function(err, result) {
             if (err) {
@@ -18,12 +19,86 @@ module.exports = function(app) {
     
     app.get('/rate/:rate_id', function(req, res) {
         const rateId = req.params['rate_id']
-        var sql = "select * from rating where id = '" + rateId + "' ";
+        var sql = "select * from rating where id = '" + rateId + "' and deleted = 0 ";
         connection.query(sql, function(err, result) {
             if (err) {
                 throw err;
             }
-            res.send(...result);
+            if(result.length > 0){
+                res.send(...result);
+            }else {
+                res.send({message: 'not found'})
+            }
+        });
+    })
+
+    app.get('/rate/product/:product_id', function(req, res) {
+        const productId = req.params['product_id']
+        var sql = "select * from rating where productid = '" + productId + "' and deleted = 0 ";
+        connection.query(sql, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            if(result.length > 0){
+                res.send(result);
+            }else {
+                res.send({message: 'not found'})
+            }
+        });
+    })
+
+    // command api
+    app.post('/rate/create_rate', urlencodeParser, function(req, res) {
+        const userId = req.body.userId ?? null;
+        const productId = req.body.productId ?? null;
+        const content = req.body.content ?? null;
+        const rating = req.body.rating ?? null;
+
+        var sql = `insert into rating(userid, productid, rating, content, deleted, created_at, updated_at) 
+        values('${userId}', '${productId}', ${rating}, '${content}', 0, '${TimeNow()}', '${TimeNow()}')`;
+
+        connection.query(sql, function(err, result) {
+            if (err) {
+                res.json({type: 'Create error', message: err.message})
+            }else {
+                res.json({type: 'Create success'})
+            }
+        });
+    })
+
+    app.post('/rate/update_rate/:rate_id', urlencodeParser, function(req, res) {
+        const rateId = req.params['rate_id']
+
+        const content = req.body.content ?? null;
+        const rating = req.body.rating ?? null;
+
+        var sql = `UPDATE rating
+        SET content =' ${content}',
+        rating = ${rating},
+        updated_at = '${TimeNow()}'
+        WHERE id = '${rateId}'`;
+
+        connection.query(sql, function(err, result) {
+            if (err) {
+                res.json({type: 'Update error', message: err.message})
+            }else {
+                res.json({type: 'Update success'})
+            }
+        });
+    })
+
+    app.post('/rate/delete_rate/:rate_id', urlencodeParser, function(req, res) {
+        const rateId = req.params['rate_id']
+        var sql = `UPDATE rating
+        SET deleted = 1
+        WHERE id = '${rateId}'`;
+
+        connection.query(sql, function(err, result) {
+            if (err) {
+                res.json({type: 'Delete error', message: err.message})
+            }else {
+                res.json({type: 'Delete success'})
+            }
         });
     })
 }
